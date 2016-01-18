55e3a38bac5ef4061a506a649ba3d0956a238d3b
Daniel Arguedas <daac93@gmail.com>
2016-01-18 19:30:08 +0000

    Refactor for article recovery

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     
     def index
         @articles = Article.all
@@ -11,7 +12,6 @@ class ArticlesController < ApplicationController
     
     
     def edit
-        @article = Article.find(params[:id])    
     end
     
     
@@ -27,7 +27,6 @@ class ArticlesController < ApplicationController
     
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully udpated"
             redirect_to article_path(@article)
@@ -38,12 +37,10 @@ class ArticlesController < ApplicationController
     
     
     def show
-        @article = Article.find(params[:id])
     end
     
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
@@ -51,6 +48,10 @@ class ArticlesController < ApplicationController
     
     
     private
+        def set_article
+           @article = Article.find(params[:id]) 
+        end
+    
         def article_params
             params.require(:article).permit(:title, :description)
         end
