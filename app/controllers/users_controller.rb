789fd09b85b5e054c9ac06e635416e18616f26e4
Daniel Arguedas <daac93@gmail.com>
2016-01-21 15:25:07 +0000

    Authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update]
     
     def index
         @users = User.paginate(page: params[:page], per_page: 3)
@@ -38,7 +39,7 @@ class UsersController < ApplicationController
     def destroy
        @user.destroy
        flash[:danger] = "Your account has been deleted."
-       redirect_to articles_path
+       redirect_to root_path
     end
     
     private
@@ -48,5 +49,12 @@ class UsersController < ApplicationController
     
     def user_params
        params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You can only edit your profile"
+            redirect_to root_path
+        end
     end
 end
