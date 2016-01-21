3494be80848bbaa246a0a0b99ad9ce333d332604
Daniel Arguedas <daac93@gmail.com>
2016-01-21 15:25:31 +0000

    Added current user as article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -61,7 +61,7 @@ class ArticlesController < ApplicationController
         
         def require_same_user
            if current_user != @article.user
-               flash[:danger] = "You can only edit your own article"
+               flash[:danger] = "You can only edit your own article."
                redirect_to root_path
            end
         end
